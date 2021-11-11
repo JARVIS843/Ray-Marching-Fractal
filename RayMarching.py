9f92f4933c22a603836472ecdc4da84546840799
Jarvis H Yang <jarvis.h.yang@outlook.com>
2021-11-11 22:51:51 +0800

    Completed basic camera movements
    Camera will move along its direction;
    Enabled acceleration and faster traverse option

--- RayMarching.py
+++ RayMarching.py
@@ -1,7 +1,7 @@
 #import pygame and essential libraries
 import pygame , sys , math
 from pygame.locals import *
-
+import numpy as np
 
 #import OpenGL
 from OpenGL.GL import *
@@ -15,7 +15,11 @@ clock = pygame.time.Clock()
 
 FPSLim = 300
 
+Velocity = 0.01;
 
+SpeedUpFactor = 10;
+
+#Temp Tests
 vertices= (
     (1, -1, -1),
     (1, 1, -1),
@@ -58,6 +62,7 @@ if __name__ == "__main__":
     pygame.init()
     
     scree = pygame.display.set_mode(Display_Size, DOUBLEBUF|OPENGL)
+    pygame.mouse.set_visible(False);
     
     glMatrixMode(GL_PROJECTION)
     gluPerspective(110 , Display_Size[0]/Display_Size[1] , 0.01 , 50)
@@ -65,17 +70,15 @@ if __name__ == "__main__":
     gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
     viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
     glLoadIdentity()
-
     glTranslatef(0,0,-10)
-    
-    
+
     # init mouse movement and center mouse on screen
     displayCenter = [scree.get_size()[i] // 2 for i in range(2)]
     mouseMove = [0, 0]
     pygame.mouse.set_pos(displayCenter)
 
     up_down_angle = 0.0
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -89,30 +92,40 @@ if __name__ == "__main__":
 
         # get keys
         keypress = pygame.key.get_pressed()
-        #mouseMove = pygame.mouse.get_rel()
+        
         # init model view matrix
         glLoadIdentity()
-        # apply the look up and down
-        up_down_angle += mouseMove[1]*0.1
-        glRotatef(up_down_angle, 1.0, 0.0, 0.0)
         # init the view matrix
         glPushMatrix()
         glLoadIdentity()
-        # apply the movment 
+
+        #Basic Controls
+        acc = 1
+        if keypress[pygame.K_SPACE]:
+                acc = SpeedUpFactor
         if keypress[pygame.K_w]:
-            glTranslatef(0,0,0.1)
+            glTranslatef(0,0,Velocity * acc)
             
         if keypress[pygame.K_s]:
-            glTranslatef(0,0,-0.1)
+            glTranslatef(0,0,-Velocity * acc)
             
         if keypress[pygame.K_d]:
-            glTranslatef(-0.1,0,0)
+            glTranslatef(-Velocity * acc,0,0)
             
         if keypress[pygame.K_a]:
-            glTranslatef(0.1,0,0)
+            glTranslatef(Velocity * acc,0,0)
+
+        if keypress[pygame.K_q]:
+            glTranslatef(0,Velocity * acc,0)
+
+        if keypress[pygame.K_e]:
+            glTranslatef(0,-Velocity * acc,0)
+        else:
+            acc = 1
             
         # apply the left and right rotation
-        glRotatef(mouseMove[0]*0.1, 0.0, 1.0, 0.0)
+        glRotatef(mouseMove[1] * Velocity * SpeedUpFactor, 1.0, 0.0, 0.0)
+        glRotatef(mouseMove[0]*Velocity * SpeedUpFactor, 0.0, 1.0, 0.0)
         
         
         # multiply the current matrix by the get the new view matrix and store the final vie matrix 
@@ -123,7 +136,7 @@ if __name__ == "__main__":
         # apply view matrix
         glPopMatrix()
         glMultMatrixf(viewMatrix)
-        
+
         
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         
---
+++ Run.bat
@@ -0,0 +1,2 @@
+python .\RayMarching.py
+pause
